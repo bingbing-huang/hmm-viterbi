14bcc44e1ac5e2c1166d84c282780b110e02a36c
Bingbing Huang <bing2013@cs.washington.edu>
2017-11-10 17:14:37 -0800

    path found out!

--- hmm.py
+++ hmm.py
@@ -27,6 +27,17 @@ def calculate_probabilities(seq, emissions, transitions, probability_matrix):
         probability_matrix[1][i] = max(prob_state1_state2, prob_state2_state2)
 
 
+def perform_traceback(probability_matrix, initial_index, viterbi_path):
+    # Traverse the probability matrix backwards one column by one column
+    index = initial_index
+    for prob_state1, prob_state2 in np.fliplr(probability_matrix).T:
+        if prob_state1 > prob_state2:
+            viterbi_path[index] = 1
+        else:
+            viterbi_path[index] = 2
+        index -= 1
+
+
 def main():
     seq = load_sequence()
     emissions = get_emissions()
@@ -35,9 +46,15 @@ def main():
         state 1 (row 0): ...
         state 2 (row 1: ...
     """
-    probability_matrix = np.zeros((2, len(seq)), dtype='float64')
+    probability_matrix = np.empty((2, len(seq)), dtype='float64')
     calculate_probabilities(seq, emissions, transitions, probability_matrix)
-    print(probability_matrix)
+    viterbi_path = np.empty(len(seq), dtype='int')
+    perform_traceback(probability_matrix, len(seq) - 1, viterbi_path)
+
+    switch = False
+    for index, path in enumerate(viterbi_path):
+        if path == 2:
+            print(index)
 
 
 if __name__ == '__main__':
